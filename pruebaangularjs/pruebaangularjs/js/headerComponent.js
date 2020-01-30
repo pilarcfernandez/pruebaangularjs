bf98de842704f7465a333d356fbd1220f8c00aae
pilarc.fernandez <pilar.fernandez@admiral-eu.com>
2020-01-30 11:11:28 +0100

    routing

--- pruebaangularjs/pruebaangularjs/js/headerComponent.js
+++ pruebaangularjs/pruebaangularjs/js/headerComponent.js
@@ -1,6 +1,10 @@
+(function(angular) {
+    'use strict';
 angular.module('pruebaApp').component('headerComponent', {
     templateUrl: 'partial/header.html',
     bindings: {
         user: '='
     }
-})
+});
+})(window.angular);
+
--- pruebaangularjs/pruebaangularjs/js/main.js
+++ pruebaangularjs/pruebaangularjs/js/main.js
@@ -1,14 +1,46 @@
-angular.module('pruebaApp',[])
+angular.module('pruebaApp', ['ngRoute'])
+
+    .config(['$routeProvider', '$locationProvider',
+        function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/listProducts', {
+                    templateUrl: 'partial/listProducts.html',
+                    controller: 'ListProductsCtrl',
+                    controllerAs: 'prods'
+                })
+                .when('/fastQuote', {
+                    templateUrl: 'partial/fastQuote.html',
+                    controller: 'QuoteCtrl',
+                    controllerAs: 'quot'
+                });
+
+            $locationProvider.html5Mode({
+                enabled: true,
+                requireBase: false
+              });
+        }])
 
     .controller('HeaderCtrl', function HeaderCtrl() {
         this.user = {
-            name: 'Anonymous'};
+            name: 'Anonymous'
+        };
     })
 
     .controller('MenuCtrl', function MenuCtrl() {
         this.options = [
-            { name: 'List products', url: 'productos.html', icon: ''},
-            { name: 'Hire', url: 'contratar.html', icon: '' },
+            { name: 'List products', url: 'listProducts', icon: '' },
+            { name: 'Fast Quoting', url: 'fastQuote', icon: '' },
             { name: 'My user settings', url: 'profile.html', icon: '' }
         ];
+    })
+
+    .controller('ListProductsCtrl', function ListProductsCtrl() {
+        this.products = [
+            { name: 'Anonymous', price: '' }
+        ];
+    })
+
+
+    .controller('QuoteCtrl', function QuoteCtrl() {
+        
     })
